b4591023ec92db15a6b34acf6b382382f56d7947
CircleCI <builds@circleci.com>
2021-04-21 22:22:24 +0000

    [ci skip] Generated SDK version v0.15.0 from CircleCI for build 664

--- src/main/java/GuestSDK/RegistrationsApi.java
+++ src/main/java/GuestSDK/RegistrationsApi.java
@@ -38,7 +38,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 public class RegistrationsApi {
-  String basePath = "https://us.tractionguest.com/api/v3";
+  String basePath = "https://tractionguest.ca/api/v3";
   ApiInvoker apiInvoker = ApiInvoker.getInstance();
 
   public void addHeader(String key, String value) {
